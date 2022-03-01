05f4b4a0858accb5899b5bd79332299213adcfb7
Christopher <christopher.kim43@gmail.com>
2022-03-01 09:15:46 -0500

    commit tree bb4fa81d99c27ff6a3e9d5fa4f7c32acde61447a

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+
+const textParagraph = document.querySelector('#text');
+
+document.addEventListener('DOMContentLoaded',function (){
+    return textParagraph.textContent = 'This is really cool!'
+
+});
+
